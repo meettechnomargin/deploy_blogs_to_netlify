4967bb667fe12b00a4ea9433110e840cbf567f1b
Meet Moradiya <meet.technomargin@gmail.com>
2026-02-24 21:13:37 +0530

    URL updated

--- api/posts.js
+++ api/posts.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://my-json-server.typicode.com/meettechnomargin/deploy_blogs_to_netlify"
+    baseURL: "https://my-json-server.typicode.com/meettechnomargin/deploy_blogs_to_netlify/posts"
 })
